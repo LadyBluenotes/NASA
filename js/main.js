9f880a5c2dfbd3700eb078bcbff955734a25af8c
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-05-29 20:40:53 -0700

    input title, date and description into dom

--- js/main.js
+++ js/main.js
@@ -10,24 +10,34 @@ function getFetch(){
       .then(data => {
         console.log(data)
 
-        // If image or video for data type.
-        if (data.media_type === 'image'){
-          document.querySelector('img').src = data.hdurl
-        } else if (data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
-        }
-        
-        // On button press, show image/video, description and title and then hide h1.
+        // Hide H1 on button click
+        document.querySelector('h1').setAttribute('style', 'display: none;')
+
+        // Add title, date and explanation to page
+        document.querySelector('h2').innerHTML = data.title
+        document.querySelector('h3').innerHTML = data.date
+        document.querySelector('p').innerHTML = data.explanation
+
+        // On button press, show image/video, description and title.
         document.querySelector('button').addEventListener('click', function(){
           document.querySelector('img').src = '';
           document.querySelector('iframe').src = '';
-          document.querySelector('h1').setAttribute('style', 'display: none;')
+        })
+
+        
+        // Show image or video depending on type
+        if (data.media_type === 'image'){
+          document.querySelector('img').setAttribute('style', 'display: block;')
+          document.querySelector('img').src = data.hdurl
+          document.querySelector('iframe').setAttribute('style', 'display: none;')
+        } else if (data.media_type === 'video'){
+          document.querySelector('iframe').setAttribute('style', 'display: block;')
+          document.querySelector('iframe').src = data.url
+          document.querySelector('img').setAttribute('style', 'display: none;')
         }
-      )
-      
       })
+      
       .catch(err => {
           console.log(`error ${err}`)
-      });
+      })
 }
-
